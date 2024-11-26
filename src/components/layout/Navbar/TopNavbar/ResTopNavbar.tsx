d80bc3fef84c0edf82bba7b12d4f3a7e83a2a5b3
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2024-11-26 21:13:04 +0500

    Update ResTopNavbar.tsx

--- src/components/layout/Navbar/TopNavbar/ResTopNavbar.tsx
+++ src/components/layout/Navbar/TopNavbar/ResTopNavbar.tsx
@@ -1,37 +1,86 @@
 import React from "react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import Image from "next/image";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { integralCF } from "@/styles/fonts";
 import { NavMenu } from "../navbar.types";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 const ResTopNavbar = ({ data }: { data: NavMenu }) => {
   return (
-    <div className="flex flex-col items-start">
-      {data.map((item) => (
-        <React.Fragment key={item.id}>
-          {item.type === "MenuItem" && (
-            <Link href={" "} className="mb-4">
-              {item.label}
-            </Link>
-          )}
-          {item.type === "MenuList" && (
-            <div className="mb-4 w-full">
-              <div className="text-left p-0 py-0.5 font-normal text-base">
-                {item.label}
-              </div>
-              <div className="p-4 pb-0 border-l flex flex-col">
-                {item.children.map((itemChild, idx) => (
-                  <Link key={itemChild.id} href={" "} className="w-fit py-2 text-base">
-                    {itemChild.label}
+    <Sheet>
+      <SheetTrigger asChild className=" ">
+        <Image
+          priority
+          src="/icons/menu.svg"
+          height={100}
+          width={100}
+          alt="menu"
+          className="max-w-[22px] max-h-[22px]"
+        />
+      </SheetTrigger>
+      <SheetContent side="left" className="overflow-y-auto">
+        <SheetHeader className="mb-10">
+          <SheetTitle asChild>
+            <SheetClose asChild>
+              <Link href=" " className={cn([integralCF.className, "text-2xl"])}>
+                SHOP.CO
+              </Link>
+            </SheetClose>
+          </SheetTitle>
+        </SheetHeader>
+        <div className="flex flex-col items-start">
+          {data.map((item) => (
+            <React.Fragment key={item.id}>
+              {item.type === "MenuItem" && (
+                <SheetClose asChild>
+                  <Link href={" "} className="mb-4">
+                    {item.label}
                   </Link>
-                ))}
-              </div>
-            </div>
-          )}
-        </React.Fragment>
-      ))}
-    </div>
+                </SheetClose>
+              )}
+              {item.type === "MenuList" && (
+                <div className="mb-4 w-full">
+                  <Accordion type="single" collapsible>
+                    <AccordionItem value={item.label} className="border-none">
+                      <AccordionTrigger className="text-left p-0 py-0.5 font-normal text-base">
+                        {item.label}
+                      </AccordionTrigger>
+                      <AccordionContent className="p-4 pb-0 border-l flex flex-col">
+                        {item.children.map((itemChild, idx) => (
+                          <SheetClose
+                            key={itemChild.id}
+                            asChild
+                            className="w-fit py-2 text-base"
+                          >
+                            <Link href={" "}>
+                              {itemChild.label}
+                            </Link>
+                          </SheetClose>
+                        ))}
+                      </AccordionContent>
+                    </AccordionItem>
+                  </Accordion>
+                </div>
+              )}
+            </React.Fragment>
+          ))}
+        </div>
+      </SheetContent>
+    </Sheet>
   );
 };
 
